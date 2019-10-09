9feadb847112c2cedc9fb71d7b07a47449c3e311
qzlyhua <qzlyhua@126.com>
2019-10-09 16:32:02 +0800

    使用css实现图片的loading效果

--- src/main/resources/static/assets/admin/js/main.js
+++ src/main/resources/static/assets/admin/js/main.js
@@ -10,13 +10,6 @@
 	$(function() {
 		var $window = $(window), $body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-		$body.addClass('is-loading');
-
-		window.setTimeout(function() {
-			$body.removeClass('is-loading');
-		}, 100);
-
 		// Touch?
 		if (skel.vars.mobile)
 			$body.addClass('is-touch');
